fa36cac81e2d309871ae5023bd125b215928a1f9
Cedric Pasteur <ccpasteur@dhcp137.dmi.ens.fr>
2011-09-30 16:36:28 +0200

    Gestion plus simple des registres
    Elle gere en plus le fait que la RAM peut couper
    les dependances

--- global/dep.ml
+++ global/dep.ml
@@ -12,22 +12,19 @@ module Graph = struct
 
   type graph =
       { g_graph : G.t;
-        mutable g_env : (G.V.t * bool) IdentEnv.t }
+        mutable g_env : G.V.t IdentEnv.t }
 
   let mk_graph () =
    { g_graph = G.create ();
      g_env = IdentEnv.empty }
 
-  let add_node g v ids is_antidep =
+  let add_node g v ids =
     let n = G.V.create v in
     G.add_vertex g.g_graph n;
-    g.g_env <- List.fold_left (fun env id -> IdentEnv.add id (n, is_antidep) env) g.g_env ids
+    g.g_env <- List.fold_left (fun env id -> IdentEnv.add id n env) g.g_env ids
 
-  let add_edge g v1 v2 is_antidep =
-    if is_antidep then
-      G.add_edge g.g_graph v2 v1
-    else
-      G.add_edge g.g_graph v1 v2
+  let add_edge g v1 v2 =
+    G.add_edge g.g_graph v1 v2
 
   let node_for_ident g x =
     IdentEnv.find x g.g_env
@@ -46,10 +43,6 @@ let def (pat, _) = match pat with
   | Evarpat id -> [id]
   | Etuplepat l -> l
 
-let is_antidep (_, e) = match e.e_desc with
-  | Eapp(OReg, _) -> true
-  | _ -> false
-
 let add id acc =
   if List.mem id acc then acc else id::acc
 (** @return the variables read by an expression
@@ -58,6 +51,12 @@ let rec read acc e =
   match e.e_desc with
     | Econst _ -> acc
     | Evar id -> add id acc
+    (* no dependency for registers *)
+    | Eapp(OReg, _) -> acc
+    (* for a RAM, only depend on the read_addr input *)
+    | Eapp(OMem (MRam, _, _, _), args) ->
+      let read_addr, _ = Misc.assert_1min args in
+      read acc read_addr
     | Eapp(_, args) -> List.fold_left read acc args
 
 let read (_, e) = read [] e
@@ -68,25 +67,25 @@ let mk_dependency_graph n =
   let init_graph () =
     let eqs = match n.n_body with | BEqs (eqs, _) -> eqs | _ -> assert false in
     let g = Graph.mk_graph () in
-    List.iter (fun eq -> Graph.add_node g eq (def eq) (is_antidep eq)) eqs;
+    List.iter (fun eq -> Graph.add_node g eq (def eq)) eqs;
     g
   in
   (** Add the dependences corresponding to the equation in the graph. *)
   let add_depends g eq =
     let node_for_eq g eq =
       let id, _ = Misc.assert_1min (def eq) in
-        fst (Graph.node_for_ident g id)
+      Graph.node_for_ident g id
     in
     let attach n1 n =
       try
-        let n2, is_antidep = Graph.node_for_ident g n in
-          Graph.add_edge g n1 n2 is_antidep
+        let n2 = Graph.node_for_ident g n in
+        Graph.add_edge g n1 n2
       with
         | Not_found -> () (*n is an input, no dependency*)
     in
     let node = node_for_eq g eq in
     let ids = read eq in
-      List.iter (attach node) ids
+    List.iter (attach node) ids
   in
   let g = init_graph () in
   let eqs = match n.n_body with | BEqs (eqs, _) -> eqs | _ -> assert false in
