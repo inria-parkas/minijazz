92bac95ff29dae61c9a5cbb03e62caab5bc1c25a
Cédric Pasteur <cedric.pasteur@ens.fr>
2012-12-05 17:33:00 +0100

    Commentaires

--- netlist/netlist_ast.ml
+++ netlist/netlist_ast.ml
@@ -1,5 +1,6 @@
 type ident = string
 
+(* Environment using ident as key *)
 module Env = struct
   include Map.Make(struct
     type t = ident
@@ -15,28 +16,35 @@ type value = VBit of bool | VBitArray of bool array
 
 type binop = Or | Xor | And | Nand
 
+(* argument of operators (variable or constant) *)
 type arg =
-    | Avar of ident
-    | Aconst of value
+    | Avar of ident (* x *)
+    | Aconst of value (* constant *)
 
+(* Expressions (see MiniJazz documentation for more info on the operators) *)
 type exp =
-    | Earg of arg
-    | Ereg of ident
-    | Enot of arg
-    | Ebinop of binop * arg * arg
-    | Emux of arg * arg * arg
+    | Earg of arg (* a: argument *)
+    | Ereg of ident (* REG x : register *)
+    | Enot of arg (* NOT a *)
+    | Ebinop of binop * arg * arg (* OP a1 a2 : boolean operator *)
+    | Emux of arg * arg * arg (* MUX a1 a2 : multiplexer *)
     | Erom of int (*addr size*) * int (*word size*) * arg (*read_addr*)
+        (* ROM addr_size word_size read_addr *)
     | Eram of int (*addr size*) * int (*word size*)
         * arg (*read_addr*) * arg (*write_enable*)
         * arg (*write_addr*) * arg (*data*)
-    | Econcat of arg * arg
+        (* RAM addr_size word_size read_addr write_enable write_addr data *)
+    | Econcat of arg * arg (* CONCAT a1 a2 : concatenation of arrays *)
     | Eslice of int * int * arg
+      (* SLICE i1 i2 a : extract the slice of a between indices i1 and i2 *)
     | Eselect of int * arg
+      (* SELECT i a : ith element of a *)
 
+(* equations: x = exp *)
 type equation = ident * exp
 
 type program =
-    { p_eqs : equation list;
-      p_inputs : ident list;
-      p_outputs : ident list;
-      p_vars : ty Env.t; }
+    { p_eqs : equation list; (* equations *)
+      p_inputs : ident list; (* inputs *)
+      p_outputs : ident list; (* outputs *)
+      p_vars : ty Env.t; } (* maps variables to their types*)
