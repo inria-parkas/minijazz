a9091554b3eb0456e6682c996ffbb80fa607f111
Cédric Pasteur <cedric.pasteur@ens.fr>
2012-12-05 17:46:49 +0100

    Documentation

--- netlist/netlist.ml
+++ netlist/netlist.ml
@@ -6,6 +6,8 @@ let find_file filename =
   with
     | _ -> raise (Parse_error ("No such file '"^filename^"%s'"))
 
+(** [read_file filename] reads the [filename] file and outputs the corresponding
+    Netlist_ast.program.*)
 let read_file filename =
   let ic = find_file filename in
   let lexbuf = Lexing.from_channel ic in
@@ -15,3 +17,10 @@ let read_file filename =
   with
     | e -> raise (Parse_error ("Syntax error (exception: "^(Printexc.to_string e)^")"))
 
+(** [print_program oc p] prints the program [p] on the output channel [oc].
+    For instance, to print on the standard output, use [print_program stdout p].
+    To print to a file named 'filename', use the following:
+        let out = open_out filename in
+        print_program out p
+*)
+let print_program = Netlist_printer.print_program
