b17661228d16774afd783577afaf3cad1921042b
Timothy Bourke <tim@tbrk.org>
2018-10-08 21:54:29 +0200

    Re-add scheduler with TODOs

---
+++ netlist/scheduler.ml
@@ -0,0 +1,9 @@
+open Netlist_ast
+open Graph
+
+exception Combinational_cycle
+
+let read_exp eq = failwith "Scheduler.read_exp: Non implementé"
+
+let schedule p = failwith "Scheduler.schedule: Non implementé"
+
